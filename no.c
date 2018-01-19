be267a0e47a7dd6eb7fa68ee07ac51cf6e21c94c
santhosh001 <31506189+santhosh001@users.noreply.github.com>
2018-01-19 10:45:23 +0000

    Create no.c

---
+++ no.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+printf("enter the three different number");
+scanf("%d,%d,%d",&a,&b,&c);
+if(a>=b && a>=c)
+{
+printf("largest number");
+}
+if(b>=c && b>=a);
+{
+printf("largest number");
+}
+if(c>=a && c>=b)
+{
+printf("largest number");
+}
+}
