92a870f196a47056ee0d1ac2c5cbd456bd283d68
santhosh001 <31506189+santhosh001@users.noreply.github.com>
2018-01-21 13:31:14 +0530

    Create sa.c

---
+++ sa.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+int s, t, b= 0;
+scanf("%d",&s);
+printf("Input\n%d\n",s);
+for(t=2; t<=s/2; t++)
+{
+if(s%t==0)
+{
+b=1;
+break;
+}
+}
+if (b==0)
+{
+printf("Output\nYes\n");
+} 
+else
+{
+printf("Output\nNo");
+}    
+return 0;
+}
