b4d4e1c66d29f757cae1c4121307eee79dc801a2
santhosh001 <31506189+santhosh001@users.noreply.github.com>
2018-01-21 14:18:11 +0530

    Create haa.c

---
+++ haa.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+
+int main(void) {
+int a[100],n,i,b,max;
+printf("\n enter the no. of values");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+	scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+for(b=0;b<n;b++)
+{
+	if(a[b]>a[i])
+		max=a[j];
+}
+}
+printf("\n the max value is %d ", max);
+	return 0;
+}
+}
