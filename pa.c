93ff78c63eaad9babc537f69fe3d090c94f313a7
santhosh001 <31506189+santhosh001@users.noreply.github.com>
2018-01-21 14:41:34 +0530

    Create pa.c

---
+++ pa.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int n,i,t1=0,t2=1;
+printf("enter the number");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+printf("%d",t)
+nt=t1+t2;
+t1=t2;
+t2=nt;
+}
+return 0;
+}
+}
