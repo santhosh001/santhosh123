ac412b43d124c361abbce5b57bf816c34a0b8878
santhosh001 <31506189+santhosh001@users.noreply.github.com>
2018-01-21 14:35:47 +0530

    Create eee.c

---
+++ eee.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+char sentence[30];
+int i,ct=1;
+scanf("%s",&sentence);
+for(i=0;i<sentena[i]!=null;i++)
+{
+if(sentena[i]='@'||sentena[i]='#'||sentena[i]='$'||sentena[i]='&')
+{
+ct++;
+}
+}
+printf("%d",ct);
+}
+}
