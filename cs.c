c583e319dbbe4b09cd6ec0c4f7ac207777b8b9ae
santhosh001 <31506189+santhosh001@users.noreply.github.com>
2018-01-21 14:11:42 +0530

    Create cs.c

---
+++ cs.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    char a;
+    printf("Enter the character:");
+    scanf("%c",&a);
+    if((a>=1)||(a<=9))
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("No");
+    }
