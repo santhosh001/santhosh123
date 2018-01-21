530a7241d31006c60d3c1b519c6b73990abf2e8c
santhosh001 <31506189+santhosh001@users.noreply.github.com>
2018-01-21 15:04:02 +0530

    Create us.c

---
+++ us.c
@@ -0,0 +1,23 @@
+
+import turtle
+def tree(branchLen,r):
+if branchLen > 5:
+r.forward(branchLen)
+r.right(20)
+tree(branchLen-15,r)
+r.left(40)
+tree(branchLen-15,r)
+r.right(20)
+r.backward(branchLen)
+def main():
+r = turtle.Turtle()
+myWin = turtle.Screen()
+r.left(90)
+r.up()
+r.backward(100)
+r.down()
+r.color("green")
+tree(75,r)
+myWin.exitonclick()
+main()
+}
