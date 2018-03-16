0d93aa8cd12f83938ab3bf01a738eaf596491b7c
santhosh001 <31506189+santhosh001@users.noreply.github.com>
2018-03-16 16:37:48 +0530

    Create QX.C

---
+++ QX.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+int a[],i,j;
+printf(" enter ");
+scanf("%d",&j);
+for(i=0;i<j;i++)
+{
+scanf("%d",&a[i]);
+
+for(i=0;i<j;i++)
+
+printf("%d",a[i]/j);
+}
